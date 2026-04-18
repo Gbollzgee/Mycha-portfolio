101d5ac6d824f88780c2f561e29c0b2beec9ace9
DANMOS <olanrewajugbolahan321@gmail.com>
2026-04-18 16:15:40 -0700

    Add CustomCursor component as a placeholder
    done

---
+++ scr/components/CustomCursor.tsx
@@ -0,0 +1,8 @@
+// This file is a placeholder - NeonCursor is the main cursor component
+// Keeping this for compatibility
+
+const CustomCursor = () => {
+  return null;
+};
+
+export default CustomCursor;
