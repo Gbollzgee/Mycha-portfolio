fd75e7ba999252e3c4e2a349b756fb8c46f7fb09
DANMOS <olanrewajugbolahan321@gmail.com>
2026-04-18 16:10:53 -0700

    Delete scr/components/ui/CustomCursor.tsx
    done

--- scr/components/ui/CustomCursor.tsx
+++
@@ -1,8 +0,0 @@
-// This file is a placeholder - NeonCursor is the main cursor component
-// Keeping this for compatibility
-
-const CustomCursor = () => {
-  return null;
-};
-
-export default CustomCursor;
